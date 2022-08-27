71d63b85031b58d3acf35f9fb13e8f084c3e0f70
kevin <kevinbarrios@ymail.com>
2022-08-27 15:48:09 -0500

    completed like.py

---
+++ app/models/like.py
@@ -0,0 +1,14 @@
+from db import db
+from datetime import datetime
+
+class Like(db.Model):
+    __tablename__ = 'likes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    created_at = db.Column(db.Datetime, default=datetime.now())
+    updated_at = db.Column(db.Datetime, default=datetime.now())
+
+    users = db.relationship('User', back_populates='likes')
+    posts = db.relationship('Post', back_populates='likes')
