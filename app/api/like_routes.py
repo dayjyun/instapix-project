f2ad371b0122b1ea3b29f0bd58e710f815337850
Huydu Tran <duutran87@icloud.com>
2022-08-28 16:35:05 -0700

    done with get/post/delete

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -1,24 +1,43 @@
+from cmath import log
 from flask import Blueprint
-from app.models import Like, User
+from app.models import db, Like, User, Post
 from flask_login import login_required, current_user
-from app.models import db
+
 
 like_routes = Blueprint('likes', __name__)
-curr_user = User.query.get(current_user.get_id())
 
 
 @like_routes.route('/posts/<post_id>/likes')
-# @login_required
+@login_required
 def likes_by_post(post_id):
     likes = Like.query.filter(Like.post_id == post_id)
     return {'likes': [like.to_dict() for like in likes]}
 
 
 @like_routes.route('/posts/<post_id>/likes', methods=['POST'])
+@login_required
 def like_a_post(post_id):
-    new_like = Like(user_id=curr_user.id,
+    # curr_user = User.query.get(current_user.get_id())
+    curr_user = current_user.get_id()
+    new_like = Like(user_id=curr_user,
                     post_id=post_id
                     )
     db.session.add(new_like)
     db.session.commit()
-    return new_like
+    return new_like.to_dict()
+
+
+@like_routes.route('/posts/<post_id>', methods=['DELETE'])
+@login_required
+def delete_a_like(post_id):
+    curr_user = current_user.get_id()
+    post = Post.query.get(post_id)
+
+    if post:
+        like = Like.query.filter(Like.user_id == curr_user,
+                                 Like.post_id == post_id)
+        like.delete()
+        db.session.commit()
+        return {"message": "Successfully deleted", "status_code": "200"}
+    else:
+        return {"message": "Post couldn't be found", "status_code": "404"}
