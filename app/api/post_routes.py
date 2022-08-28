ca7a54ca4b9a73a1dbcb180a64500acb91faa177
kevin <kevinbarrios@ymail.com>
2022-08-28 17:20:10 -0500

    get current user added

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,6 +1,7 @@
 from crypt import methods
 from flask import Blueprint, session
 from flask_login import login_required, current_user
+from app.api.auth_routes import authenticate
 from app.models import User
 
 post_routes = Blueprint('posts', __name__, url_prefix='/posts')
@@ -10,36 +11,35 @@ post_routes = Blueprint('posts', __name__, url_prefix='/posts')
 @post_routes.route('/')
 @login_required
 def get_posts():
-#     # c_user = User.query.get(current_user.id)
-#     c_user = current_user()
-#     # if I am logged in
-#     # if current_user.is_authenticated
-#     # import people I follow and verify I am following user
-#     # return the posts of the people I follow
+    c_user = User.query.get(current_user.get_id())
     pass
-#     return f"{c_user}"
 
 @post_routes.route('/<post_id>')
 @login_required
 def post_details(post_id):
+    c_user = User.query.get(current_user.get_id())
     pass
 
 @post_routes.route('/<post_id>/edit')
 @login_required
 def get_post_edit_form(post_id):
+    c_user = User.query.get(current_user.get_id())
     pass
 
 @post_routes.route('/<post_id>', methods=["PUT"])
 @login_required
 def update_post_edit_form():
+    c_user = User.query.get(current_user.get_id())
     pass
 
 @post_routes.route('/', methods=["POST"])
 @login_required
 def create_post():
+    c_user = User.query.get(current_user.get_id())
     pass
 
 @post_routes.route('/<post_id>', methods=['DELETE'])
 @login_required
 def delete_post(post_id):
+    c_user = User.query.get(current_user.get_id())
     pass
