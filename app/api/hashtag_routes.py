4e0eb9b1809d8592ca24dca4ea1f69645effeedd
Huydu Tran <duutran87@icloud.com>
2022-08-28 19:58:48 -0700

    test

--- app/api/hashtag_routes.py
+++ app/api/hashtag_routes.py
@@ -1,1 +1,13 @@
-from flask import Blueprint, current_app
+from flask import Blueprint
+from app.models import Hashtag
+
+hashtag_routes = Blueprint('hashtags', __name__)
+
+# GET ALL HASHTAGS
+
+
+@hashtag_routes.route('/hashtags')
+def get_all_hash():
+    all = Hashtag.query.all()
+    all = [hashtag.to_dict() for hashtag in all]
+    return {'Hashtag': all}
--- app/models/hashtag.py
+++ app/models/hashtag.py
@@ -4,9 +4,10 @@ from datetime import datetime
 post_hashtag = db.Table(
     "post_hashtags",
     db.metadata,
-    db.Column('hashtag_id', db.ForeignKey('hashtags.id'), primary_key = True),
-    db.Column('post_id', db.ForeignKey('posts.id'), primary_key = True)
+    db.Column('hashtag_id', db.ForeignKey('hashtags.id'), primary_key=True),
+    db.Column('post_id', db.ForeignKey('posts.id'), primary_key=True)
 )
+
 
 class Hashtag(db.Model):
     __tablename__ = 'hashtags'
@@ -16,4 +17,9 @@ class Hashtag(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, default=datetime.now())
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'hashtag_value': self.hashtag_value
+        }
     # posts = db.relationship('Post', secondary=post_hashtag, back_populates='hashtags')
