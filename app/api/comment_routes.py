2728b8db51e60ee6be28caec95b49fb70180777c
Jan <janjovellanos@gmail.com>
2022-08-29 18:20:41 -0700

    added 404/401 handling on routes

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -23,22 +23,26 @@ def get_comment(comment_id):
     comment = Comment.query.get(comment_id)
     if comment:
         return comment.to_dict()
-        # print(db.session.current_user)
     return jsonify(message="Comment couldn't be found", statusCode=404)
 
 # create a comment providing user_id, post_id, and body
+# change this route to a POST at --> /posts/<int:post_id>/comments
 @comment_routes.route('/new', methods=['GET', 'POST'])
 @login_required
 def create_comment():
     form = CreateCommentForm()
-    if form.validate_on_submit():
-        data = form.data
-        comment = Comment(user_id=data['user_id'],
-                          post_id=data['post_id'],
-                          body=data['body'])
-        db.session.add(comment)
-        db.session.commit()
-        return comment.to_dict()
+
+    if request.method != 'GET':
+        if form.validate_on_submit():
+            data = form.data
+            comment = Comment(user_id=current_user.id,
+                              post_id=data['post_id'],
+                              body=data['body'])
+
+            db.session.add(comment)
+            db.session.commit()
+
+            return comment.to_dict()
     return render_template('create_comment_form.html', form=form)
 
 # edit a comment using comment_id, and providing a body and updating update_at
@@ -47,13 +51,22 @@ def create_comment():
 def edit_comment(comment_id):
     form = EditCommentForm()
 
-    if not request.method == 'GET':
+    if request.method != 'GET':
         comment = Comment.query.get(comment_id)
+
+        # check if comment exists
+        if not comment:
+            return jsonify(message="Comment couldn't be found", statusCode=404)
+
+        #check if user is authenticated
+        if current_user.id != comment.user_id:
+            return jsonify(message='Authentication required', statusCode='401')
+
+        # edit comment
         if form.validate_on_submit():
             data = form.data
             comment.body = data['body']
             comment.updated_at = datetime.now()
-            # print(comment.body)
 
             db.session.add(comment)
             db.session.commit()
@@ -61,6 +74,7 @@ def edit_comment(comment_id):
             return comment.to_dict()
     return render_template('edit_comment_form.html', form=form)
 
+# delete a comment using comment_id
 @comment_routes.route('/<int:comment_id>', methods=['DELETE'])
 @login_required
 def delete_comment(comment_id):
@@ -72,8 +86,14 @@ def delete_comment(comment_id):
 
 
 
-# fetch('http://localhost:5000/api/comments/7', {
-#   method: 'DELETE'
+# fetch('localhost:5000/api/comments/2/edit', {
+#   method: 'PUT',
+#   body: JSON.stringify({
+#     body: 'THIS IS AN EDIT',
+#   }),
+#   headers: {
+#     'Content-type': 'application/json; charset=UTF-8'
+#   }
 # })
-#   .then(res => res.json())
-#   .then(console.log)
+# .then(res => res.json())
+# .then(console.log)
