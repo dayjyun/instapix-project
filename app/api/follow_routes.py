4da038cadd93390aa4600509b1b71eab6cd7e417
Sally <sallykim13@gmail.com>
2022-08-29 23:03:23 -0500

    clean routes

--- app/api/follow_routes.py
+++ app/api/follow_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from app.models import Follow, User, db
 from flask_login import current_user, login_required
 
@@ -23,10 +23,8 @@ def get_follows_for_user(user_id):
     if user:
         return {'Followers': [user for user in follows_list]}
     else:
-        return {
-            "message": "User couldn't be found",
-            "statusCode": 404
-            }
+        return jsonify(message='User could not be found.', status_code=404)
+
 
 
 # Get all accounts that the user follows
@@ -34,9 +32,7 @@ def get_follows_for_user(user_id):
 @login_required
 def get_users_follows(user_id):
     user = User.query.get(user_id)
-
-    follows = Follow.query\
-        .filter(Follow.user_id == user_id)
+    follows = Follow.query.filter(Follow.user_id == user_id)
 
     follows_list = []
     for follow in follows:
@@ -47,29 +43,21 @@ def get_users_follows(user_id):
     if user:
         return {'Followers': [user for user in follows_list]}
     else:
-        return {
-            "message": "User couldn't be found",
-            "statusCode": 404
-            }
+        return jsonify(message='User could not be found.', status_code=404)
+
 
 
 #Follow a user (post)
 @follow_routes.route('/users/<user_id>/post', methods=['GET','POST'])
 @login_required
 def follow_user(user_id):
+    user_id= int(user_id)
     user = User.query.get(user_id)
     is_already_following = Follow.query.filter(Follow.user_id == current_user.id).all()
 
-    print('----')
-    print(is_already_following)
-    print('----')
-
     for follow in is_already_following:
         if follow.follows_id == user_id:
-            return {
-            "message": "You are already following this user.",
-            "statusCode": 400
-            }
+            return jsonify(message='You are already following this user.', status_code=404)
 
     if user:
         new_follow = Follow(
@@ -78,50 +66,31 @@ def follow_user(user_id):
         )
         db.session.add(new_follow)
         db.session.commit()
-
-        return {
-            "message": "Successfully followed",
-            "statusCode": 200
-            }
+        return jsonify(message='Successfully followed.', status_code=200)
 
     else:
-        return {
-            "message": "User couldn't be found",
-            "statusCode": 404
-                }
+        return jsonify(message='User could not be found.', status_code=404)
+
 
 
 #Unfollow a user (delete)
-@follow_routes.route('/users/<user_id>/delete', methods=['DELETE'])
+@follow_routes.route('/users/<user_id>/delete', methods=['GET','DELETE'])
 @login_required
 def unfollow_user(user_id):
+    user_id= int(user_id)
     user = User.query.get(user_id)
     #get all follows for the user whose page we're on
     all_my_follows = Follow.query.filter(current_user.id == Follow.user_id).all()
 
     if user:
         for follow in all_my_follows:
-            if follow.follows_id == int(user_id):
+            if follow.follows_id == user_id:
                 my_follow = Follow.query.get(follow.id)
                 db.session.delete(my_follow)
                 db.session.commit()
+                return jsonify(message='Successfully unfollowed.', status_code=404)
 
-        # updated_follows = Follow.query.filter(current_user.id == Follow.user_id).all()
-        return {
-            "message": "Successfully unfollowed",
-            "statusCode": 200
-            }
-        # return {Follow: [follow.to_dict() for follow in updated_follows]}
+        return jsonify(message='You cannot unfollow someone you do not follow.', status_code=404)
 
     else:
-        return {
-            "message": "User couldn't be found",
-            "statusCode": 404
-                }
-
-# for follow in is_already_following:
-#         if follow.follows_id == user_id:
-#             return {
-#             "message": "You cannot unfollow someone you do not follow.",
-#             "statusCode": 400
-#             }
+        return jsonify(message='User could not be found.', status_code=404)
