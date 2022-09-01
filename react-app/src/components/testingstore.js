828213695ff6635c3cc484ce61dbd11fcbc4c1b5
Huydu Tran <duutran87@icloud.com>
2022-08-31 20:11:29 -0700

    nothing

--- react-app/src/components/testingstore.js
+++ react-app/src/components/testingstore.js
@@ -15,6 +15,12 @@ const TestingComponent = () => {
         dispatch(hashtagActions.fetchAllHashtags())
         dispatch(likeActions.fetchLike(1))
     }, [dispatch])
+
+    const handleOnClick = (e) => {
+        e.preventDefault()
+        dispatch(likeActions.like(1))
+    }
+
     return (
         <div>
             <div>
