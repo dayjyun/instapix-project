d042daf339d21065275a2bd7713bebe59bc46f00
Felipe Silva <felipesilva916@gmail.com>
2022-08-31 22:00:37 -0700

    Update follow.py

--- app/models/follow.py
+++ app/models/follow.py
@@ -1,6 +1,7 @@
 from .db import db
 from datetime import datetime
-from .user import User
+# from .user import User
+
 
 class Follow(db.Model):
     __tablename__ = 'follows'
@@ -15,11 +16,11 @@ class Follow(db.Model):
     # posts = db.relationship('Post', back_populates='follows')
     # follows = db.relationship('User', back_populates='follows')
     # followed_by = db.relationship('User', back_populates='follows')
-    def all_followers(self):
-        return User.query.filter(User.id==self.user_id).all()
+    # def all_followers(self):
+    #     return User.query.filter(User.id==self.user_id).all()
 
-    def all_follows(self):
-        return User.query.filter(User.id==self.follows_id).all()
+    # def all_follows(self):
+    #     return User.query.filter(User.id==self.follows_id).all()
 
     # def all_followers(self):
     # return User.query.filter(User.id == self.id).all()
@@ -31,23 +32,22 @@ class Follow(db.Model):
             'follows_id': self.follows_id
         }
 
-
-    def to_dict_followers(self):
-        return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'follows_id': self.follows_id,
-            'Follower_Info': [user.follow_info() for user in self.all_followers()]
-        }
-    def to_dict_follows(self):
-        return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'follows_id': self.follows_id,
-            'Follower_Info': [user.follow_info() for user in self.all_follows()]
+    # def to_dict_followers(self):
+    #     return {
+    #         'id': self.id,
+    #         'user_id': self.user_id,
+    #         'follows_id': self.follows_id,
+    #         'Follower_Info': [user.follow_info() for user in self.all_followers()]
+    #     }
+    # def to_dict_follows(self):
+    #     return {
+    #         'id': self.id,
+    #         'user_id': self.user_id,
+    #         'follows_id': self.follows_id,
+    #         'Follower_Info': [user.follow_info() for user in self.all_follows()]
 
     # TODO Return the ID, username, profile image, and posts from users that the current user is following *
-        }
+        # }
 
     def to_dict_following(self):
         return {
@@ -56,6 +56,6 @@ class Follow(db.Model):
             'username': self.users.username,
             'profile_image': self.users.profile_image,
             'Posts': [post.to_dict() for post in self.users.following_posts()],
-}
+        }
 
 # users_table = db.session.query(Follow, User).join(User).all()
