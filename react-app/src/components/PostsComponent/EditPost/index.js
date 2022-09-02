2e460c5adefd05a11e611fdefe580eb044eca523
kevin <kevinbarrios@ymail.com>
2022-09-02 17:49:17 -0500

    edits

---
+++ react-app/src/components/PostsComponent/EditPost/index.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import { editPost } from "../../../store/posts";
+
+function EditPostForm(){
+    const dispatch = useDispatch()
+    const { postId } = useParams()
+    const posts = Object.values(useSelector(state => state.posts))
+
+    useEffect(() => {
+        dispatch(editPost(+postId))
+    }, [dispatch])
+
+    const handlePostFormSubmit = async (e) => {
+        e.preventDefault();
+
+        await dispatch(
+            editPost({
+                id: postId,
+                user_id,
+                caption,
+                post_url,
+
+            })
+        )
+    }
+
+    return (
+      <div>
+        <form onSubmit={handlePostFormSubmit}>
+            <ul>
+
+            </ul>
+        </form>
+      </div>
+    );
+}
+
+export default EditPostForm
