b1b8de388d5a8a34d2cd669a71267399d84a3b42
kevin <kevinbarrios@ymail.com>
2022-09-01 20:40:40 -0500

    current progress with get post component

--- react-app/src/components/Post.js
+++ react-app/src/components/Post.js
@@ -2,33 +2,33 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getPost } from "../store/posts";
+// import * as postActions from "../store/posts";
 
 function PostComponent() {
-    const dispatch = useDispatch();
-    const { songId } = useParams();
-    const posts = Object.values(useSelector(state => state.posts));
-    const comments = posts[0]?.Post[0]?.Comments;
+  const dispatch = useDispatch();
+  const { postId } = useParams();
+  const posts = Object.values(useSelector((state) => state.posts));
+  const post = post[postId]
+  console.log(posts[0]?.Post[0].caption);
 
-    useEffect(() => {
-        dispatch[getPost(songId)]
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(getPost(+postId));
+    // dispatch(postActions.getPost(postId));
+  }, [dispatch, postId]);
 
-    return (
+  return (
+    <div>
       <div>
         <ul>
-          {posts?.map((post) => (
-            <li key={post.id}>
-              <div>{posts[0]?.Post[0].caption}</div>
+          {post?.map((p) => (
+            <li key={p.id}>
+              <div>{p[0]?.Post[0].caption}</div>
             </li>
           ))}
         </ul>
-        <ul>
-          {comments?.map((comment) => (
-            <li key={comment.id}>{comment?.body}</li>
-          ))}
-        </ul>
       </div>
-    );
+    </div>
+  );
 }
 
-export default PostComponent
+export default PostComponent;
--- react-app/src/components/testingstore.js
+++ react-app/src/components/testingstore.js
@@ -1,56 +1,51 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import * as postActions from '../store/posts'
+import * as hashtagActions from '../store/hashtags'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import * as likeActions from '../store/likes'
 
 const TestingComponent = () => {
-  const dispatch = useDispatch();
-    const posts = Object.values(useSelector((state ) => state.posts));
-    const comments = posts[0]?.Post[0]?.Comments
-    console.log(comments)
-//   useEffect(() => {
-//     dispatch(hashtagActions.fetchAllHashtags());
-//     dispatch(likeActions.fetchLike(1));
-//   }, [dispatch]);
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const hashtags = Object.values(useSelector(state => state.hashtags))
+    const likes = Object.values(useSelector(state => state.likes))
+
 
     useEffect(() => {
-        dispatch(postActions.getPost(1));
+        dispatch(hashtagActions.fetchAllHashtags())
+        dispatch(likeActions.fetchLike(1))
     }, [dispatch])
 
-//   const handleOnClick = (e) => {
-//     e.preventDefault();
-//     dispatch(likeActions.unlike(2));
-//   };
+    const handleOnClick = (e) => {
+        e.preventDefault()
+        dispatch(likeActions.unlike(2))
+    }
 
-  return (
-    <div>
-      <div>
-        {posts?.map((post) => (
-          <li key={post.id}>
-            <div>{posts[0]?.Post[0].caption}</div>
-            {/* <div>{post.post_url}</div> */}
-          </li>
-        ))}
-        {comments?.map(comment => (
-            <li key={comment.id}>{comment?.body}</li>
-        ))}
-        {/* {hashtags?.map((hashtag) => (
-          <li key={hashtag?.id}>
-            <div>hashtagid: {hashtag.id}</div>
-            <div>hashtag_value: {hashtag.hashtag_value}</div>
-          </li>
-        ))} */}
-      </div>
-      {/* <div>
-        {likes.map((like) => (
-          <div key={like.id}>
-            <li>{like.post_id}</li>
-            <li>{like.user_id}</li>
-          </div>
-        ))}
-      </div> */}
-      {/* <button onClick={handleOnClick}>LIKE</button> */}
-    </div>
-  );
-};
+    return (
+        <div>
+            <div>
+                {hashtags?.map((hashtag) => (
+                    <li key={hashtag?.id}>
+                        <div>
+                            hashtagid: {hashtag.id}
+                        </div>
+                        <div>
+                            hashtag_value: {hashtag.hashtag_value}
+                        </div>
+                    </li>
+                ))}
+            </div>
+            <div>
+                {likes.map(like => (
+                    <div key={like.id}>
+                        <li>{like.post_id}</li>
+                        <li>{like.user_id}</li>
+                    </div>
+                ))}
+            </div>
+            <button onClick={handleOnClick}>LIKE</button>
+        </div>
+    )
+}
 
-export default TestingComponent;
+export default TestingComponent
