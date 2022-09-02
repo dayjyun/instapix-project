eb1fe32ace18c238a80ea38c1baacc4c7f7ce5f4
Felipe Silva <felipesilva916@gmail.com>
2022-09-01 20:41:01 -0700

    Divssss

--- react-app/src/components/PostModal/PostForm.js
+++ react-app/src/components/PostModal/PostForm.js
@@ -1,59 +1,60 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
-import './PostForm.css'
+import "./PostForm.css";
 
 function PostForm() {
-    const dispatch = useDispatch();
-    const [credential, setCredential] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState([]);
+  const dispatch = useDispatch();
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [caption, setCaption] = useState("");
+  const [post_url, setPost_url] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setErrors([]);
-        return dispatch(sessionActions.login({ credential, password })).catch(
-            async (res) => {
-                const data = await res.json();
-                if (data && data.errors) setErrors(data.errors);
-            }
-        );
-    };
-
-    return (
-        <div className="post-form-container">
-            <div className="create-new-post">
-                <p>Create new post</p>
-            </div>
-            <div>POST FORM IN HERE</div>
-            {/* <form onSubmit={handleSubmit}>
-                <ul>
-                    {errors.map((error, idx) => (
-                        <li key={idx}>{error}</li>
-                    ))}
-                </ul>
-                <label>
-                    Username or Email
-                    <input
-                        type="text"
-                        value={credential}
-                        onChange={(e) => setCredential(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Password
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </label>
-                <button type="submit">Log In</button>
-            </form> */}
-        </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      }
     );
+  };
+
+  return (
+    <div className="post-form-container">
+      <div className="create-new-post">
+        <p>Create new post</p>
+      </div>
+      <div className="caption-url-container">
+        <form className="form-here" onSubmit={handleSubmit}>
+          <div>
+            <ul>
+              {errors.map((error, idx) => (
+                <li key={idx}>{error}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="form-item">
+            <label>Caption</label>
+            <input
+              type="text"
+              value={caption}
+              onChange={(e) => setCaption(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-item">
+            <label>Image URL:</label>
+            <input value={post_url} type="text" />
+          </div>
+
+          <button type="submit">Post</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default PostForm;
