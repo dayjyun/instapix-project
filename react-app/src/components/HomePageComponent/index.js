7e408c616c3654d64ba898bff522b625d2906ed6
Huydu Tran <duutran87@icloud.com>
2022-09-03 16:21:57 -0700

    almost done with login page

--- react-app/src/components/HomePageComponent/index.js
+++ react-app/src/components/HomePageComponent/index.js
@@ -1,10 +1,20 @@
-import { useState, useDispatch } from "react"
+import { useState, useDispatch, useEffect } from "react"
 import { useHistory } from "react-router-dom"
 import { useSelector } from "react-redux"
+import './HomePageComponent.css'
 
 
 const HomePageComponent = () => {
     const sessionUser = useSelector(state => state.session.user)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [toggle, setToggle] = useState(true)
+
+    useEffect(() => {
+        if (email && password) {
+            setToggle(false)
+        }
+    }, [email, password])
 
     if (sessionUser) {
         return (
@@ -14,7 +24,49 @@ const HomePageComponent = () => {
         return (
             <div className="logged-out-container">
                 <div className="logged-out-content-container">
-                    
+                    <h1>hey</h1>
+                    <div className="login-form-container">
+                        <div className="login-form">
+                            <h1>Instapix</h1>
+                            <div className="login-input-container">
+                                <div className="login-box">
+                                    <div className='email'>
+                                        <p>Email</p>
+                                    </div>
+                                    <div className="password">
+                                        <input
+                                            type='text'
+                                            value={email}
+                                            onChange={e => setEmail(e.target.value)}
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="login-input-container">
+                                <div className="login-box">
+                                    <div className='email'>
+                                        <p>Password</p>
+                                    </div>
+                                    <div className="password">
+                                        <input
+                                            type='password'
+                                            value={password}
+                                            onChange={e => setPassword(e.target.value)}
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="login-input-container">
+                                <button disabled={toggle} className="login-button">Log In</button>
+                            </div>
+                        </div>
+                        <div className="sign-up-link">
+                            {/* sign up modal goes here */}
+                            <h1>bottom</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
