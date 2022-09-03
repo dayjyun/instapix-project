289ad018db2a982ed7438142583bcd877b066b4d
kevin <kevinbarrios@ymail.com>
2022-09-02 23:03:56 -0500

    current updates with edit post button

--- react-app/src/components/PostsComponent/EditPost/EditPostBtn.js
+++ react-app/src/components/PostsComponent/EditPost/EditPostBtn.js
@@ -1,16 +1,32 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Modal } from "../../../context/Modal";
+import { deletePost } from "../../../store/posts";
 import PostMenu from "./PostMenu";
 
 function EditPostBtn() {
+    const dispatch = useDispatch()
+    // postId?
   const [showModal, setShowModal] = useState(false);
+
+  const handleDeletePostBtn = (e) => {
+    e.preventDefault()
+    dispatch(deletePost(1))
+  }
+
+  const handleCancelBtn = (e) => {
+    e.preventDefault();
+    setShowModal(false);
+  };
 
   return (
     <>
-      <button onClick={() => setShowModal == true ? setShowModal(false) : setShowModal(true)}>...</button>{" "}
+      <button onClick={() => setShowModal(true)}>Menu Button...</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
+          <button onClick={handleDeletePostBtn}>Delete</button>
           <PostMenu />
+          <button onClick={handleCancelBtn}>Cancel</button>
         </Modal>
       )}
     </>
--- react-app/src/components/PostsComponent/EditPost/PostMenu.js
+++ react-app/src/components/PostsComponent/EditPost/PostMenu.js
@@ -5,21 +5,20 @@ import EditPostForm from "./EditPostForm";
 function PostMenu() {
   const [showModal, setShowModal] = useState(false);
 
-  const handleCancelBtn=(e) => {
-    e.preventDefault()
-    setShowModal(false)
-  }
+  const handleCancelBtn = (e) => {
+    e.preventDefault();
+    setShowModal(false);
+  };
 
   return (
     <>
-      <button>Delete</button>
       <button onClick={() => setShowModal(true)}>Edit</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <EditPostForm />
         </Modal>
       )}
-      <button onClick={handleCancelBtn}>Cancel</button>
+      {/* <button onClick={handleCancelBtn}>Cancel</button> */}
     </>
   );
 }
