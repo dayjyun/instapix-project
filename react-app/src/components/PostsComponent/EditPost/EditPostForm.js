b30c402d9f35ba46409139b7ad9d1ae6d2c22bd7
kevin <kevinbarrios@ymail.com>
2022-09-03 15:31:13 -0500

    after pull from dev

--- react-app/src/components/PostsComponent/EditPost/EditPostForm.js
+++ react-app/src/components/PostsComponent/EditPost/EditPostForm.js
@@ -13,7 +13,7 @@ function EditPostForm({ setShowMenuButtons, setShowEditPost }) {
   const post_caption = posts?.map((post) => post?.caption);
   const post_image = posts?.map((post) => post?.post_url);
   const [caption, setCaption] = useState(post_caption);
-  
+
   const handlePostFormSubmit = (e) => {
     e.preventDefault();
 
