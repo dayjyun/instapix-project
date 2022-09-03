a1ef851fc843c3cf6ba9a9e2a4820777308cf6b9
kevin <kevinbarrios@ymail.com>
2022-09-03 18:34:40 -0500

    current updates with explorer css

--- react-app/src/components/PostsComponent/ExplorerPosts/index.js
+++ react-app/src/components/PostsComponent/ExplorerPosts/index.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { loadAllPosts } from "../../../store/posts";
+import "./index.css";
 
 function ExplorerPosts() {
   const dispatch = useDispatch();
@@ -12,17 +14,18 @@ function ExplorerPosts() {
   }, [dispatch]);
 
   return (
-    <div>
-      <div>
-        <ul>
-          {posts?.map((post) => (
-            <li key={post?.id}>
-              <div>{post?.post_url}</div>
-              <div>{post?.caption}</div>
+    <div className="explorer-page">
+      <div className="explorer-post">
+        {posts?.map((post) => (
+          <li key={post?.id} className="explorer-card">
+            <Link exact to={{ pathname: `/posts/${post?.id}` }}>
+              <img className="explorer-image" src={post?.post_url} />
+              {/* <div>{post?.caption}</div> */}
               <div>{post?.likes} Likes</div>
-            </li>
-          ))}
-        </ul>
+              <div>{post?.num_comments} Comments</div>
+            </Link>
+          </li>
+        ))}
       </div>
     </div>
   );
