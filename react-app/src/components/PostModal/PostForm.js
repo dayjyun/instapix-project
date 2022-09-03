d10bf8fd04e3ba284528875e7c93135208cdb2c8
kevin <kevinbarrios@ymail.com>
2022-09-03 01:30:41 -0500

    updates for post form js

--- react-app/src/components/PostModal/PostForm.js
+++ react-app/src/components/PostModal/PostForm.js
@@ -6,83 +6,92 @@ import { createPost } from "../../store/posts";
 import { Redirect, useHistory } from "react-router-dom";
 import * as postActions from "../../store/posts";
 
+function PostForm({ closeModal }) {
+  const dispatch = useDispatch();
+  const [errors, setErrors] = useState([]);
+  const [caption, setCaption] = useState("");
+  const [postUrl, setPostUrl] = useState("");
+  const history = useHistory();
+  const posts = Object.values(useSelector((state) => state.posts));
+  const currUser = useSelector((state) => state.session.user);
 
-function PostForm() {
-    const dispatch = useDispatch();
-    const [errors, setErrors] = useState([]);
-    const [caption, setCaption] = useState("");
-    const [postUrl, setPostUrl] = useState("");
-    const history = useHistory();
-    const posts = Object.values(useSelector((state) => state.posts));
-    const currUser = useSelector(state => state.session.user)
-    console.log(currUser)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    dispatch(postActions.createPost({ caption, postUrl })).then(() => {
+      closeModal();
+    });
+    setPostUrl("");
+    setCaption("");
+    const postId = posts[posts.length - 1]?.id;
+    // history.push(`/posts/${postId}`)
+  };
 
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setErrors([]);
-        dispatch(postActions.createPost({ caption, postUrl }));
-        const postId = posts[posts.length - 1]?.id
-        history.push(`/posts/${postId}`)
-    };
-
-    return (
-        <div className="post-form-container">
-            <div className="create-new-post">
-                <p>Create new post</p>
-                {caption && (
-                    <button type='submit'>Share</button>
-                )}
+  return (
+    <div className="post-form-container">
+      <div className="create-new-post">
+        <p>Create new post</p>
+        {caption && (
+          <div className="share-button-container">
+            <button type="submit" onClick={handleSubmit}>
+              Share
+            </button>
+          </div>
+        )}
+      </div>
+      <div className="form-container">
+        <div className="form-image">{postUrl && <img src={postUrl}></img>}</div>
+        <div className="post-caption-form-container">
+          <div className="post-caption-form-area">
+            <div className="post-username">
+              <img
+                className="profile-img-circle-container form-profile-img"
+                src={currUser?.profile_image}
+                alt="profileImage"
+              ></img>
+              <h3>{currUser?.username}</h3>
             </div>
-            <div className="form-container">
-                <div className="form-image">
-                    {postUrl && (
-                        <img style={{ width: '32vw', height: '64.4vh' }} src={postUrl}></img>
-                    )}
-                </div>
-                <div className="post-caption-form-container">
-                    <div className="post-caption-form-area">
-                        <div className='post-username'>
-                            <img className="profile-img-circle-container form-profile-img" src={currUser?.profile_image} alt="profileImage"></img>
-                            <h3>{currUser?.username}</h3>
-                        </div>
-                        <form onSubmit={handleSubmit}>
-                            <ul>
-                                {errors.map((error, idx) => (
-                                    <li key={idx}>{error}</li>
-                                ))}
-                            </ul>
-                            <label>
-                                <textarea className="text-area"
-                                    value={caption}
-                                    placeholder='Write a caption...'
-                                    onChange={(e) => setCaption(e.target.value)}
-                                    maxLength='2000'
-                                    required
-                                />
-                            </label>
-                        </form>
-                        <form onSubmit={handleSubmit}>
-                            <label>
-                                Post Url
-                                <input
-                                    type="text"
-                                    value={postUrl}
-                                    onChange={(e) => setPostUrl(e.target.value)}
-                                />
-                            </label>
-                            {/* <button type="submit">Post</button> */}
-                        </form>
-                    </div>
-                </div>
-            </div>
+            <form>
+              <ul>
+                {errors.map((error, idx) => (
+                  <li key={idx}>{error}</li>
+                ))}
+              </ul>
+              <label>
+                <textarea
+                  className="text-area"
+                  value={caption}
+                  placeholder="Write a caption..."
+                  onChange={(e) => {
+                    setCaption(e.target.value);
+                  }}
+                  maxLength="2000"
+                  required
+                />
+              </label>
+            </form>
+            <form>
+              <div className="char-count-area">
+                <input
+                  className="image-url"
+                  type="text"
+                  value={postUrl}
+                  placeholder="Image Url"
+                  onChange={(e) => setPostUrl(e.target.value)}
+                />
+                <p style={{ color: "lightgray" }}>{caption.length}/2000</p>
+              </div>
+              {/* <button type="submit">Post</button> */}
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-
-{/* <label>
+{
+  /* <label>
                                 Post Url
                                 <input
                                     type="text"
@@ -90,6 +99,7 @@ function PostForm() {
                                     onChange={(e) => setPostUrl(e.target.value)}
                                 />
                             </label>
-                            <button type="submit">Post</button> */}
+                            <button type="submit">Post</button> */
+}
 
 export default PostForm;
