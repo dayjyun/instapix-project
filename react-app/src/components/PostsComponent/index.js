5f0405f5bcb60e5249f8b427e8bfa6bd72db25a4
kevin <kevinbarrios@ymail.com>
2022-09-03 21:23:23 -0500

    current updates with post by id css

--- react-app/src/components/PostsComponent/index.js
+++ react-app/src/components/PostsComponent/index.js
@@ -3,34 +3,46 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getPost } from "../../store/posts";
 import EditPostBtn from "./EditPost/EditPostBtn";
+import "./Posts.css";
 
 function PostComponent() {
   const dispatch = useDispatch();
   const { postId } = useParams();
   const posts = Object.values(useSelector((state) => state.posts));
+  const currUser = useSelector((state) => state.session.user);
 
   useEffect(() => {
     dispatch(getPost(+postId));
   }, [dispatch]);
 
   return (
-    <div>
-      <EditPostBtn />
-      <div>
-        <ul>
-          {posts?.map((post) => (
-            <li key={post?.id}>
-              <img
-                style={{ width: "500px", height: "500px" }}
-                src={post?.post_url}
-                alt="post_image"
-              ></img>
+    <div className="post-page">
+      {posts?.map((post) => (
+        <li key={post?.id} className="post-id-card">
+          <img
+            style={{ width: "500px", height: "500px" }}
+            src={post?.post_url}
+            alt="post_image"
+          ></img>
+          <div className="page-post-details">
+            <div className="post-details-top">
+              <div className="post-username">
+                <img
+                  className="profile-img-circle-container form-profile-img"
+                  src={currUser?.profile_image}
+                  alt="profileImage"
+                ></img>
+                <h3>{currUser?.username}</h3>
+              </div>
+              <EditPostBtn />
+            </div>
+            <div className="post-user-details">
               <div>{post?.caption}</div>
               <div>{post?.likes} Likes</div>
-            </li>
-          ))}
-        </ul>
-      </div>
+            </div>
+          </div>
+        </li>
+      ))}
     </div>
   );
 }
