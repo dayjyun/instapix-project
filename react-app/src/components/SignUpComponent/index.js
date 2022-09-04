4c859429306ddb8a7f2a21ee4d0429aefd1c3d7b
Felipe Silva <felipesilva916@gmail.com>
2022-09-04 11:01:46 -0700

    fixed sign up page

--- react-app/src/components/SignUpComponent/index.js
+++ react-app/src/components/SignUpComponent/index.js
@@ -66,94 +66,96 @@ const SignUpUserForm = () => {
     return <Redirect to="/" />;
   }
   return (
-    <div className="signup-background">
-      <div className="signup-form-container">
-        <div className="logo-container">
-          <h1 className="logo">Instapix</h1>
-          <p>Sign up to see photos and videos from your friends.</p>
-          <button onClick={handleGuestLogin} className="signup-demo-user">
-            <i class="fa-sharp fa-solid fa-user"></i> Log in as a demo user
-          </button>
-        </div>
-        <div className="auth-separator">
-          <span>OR</span>
-        </div>
-        <div id="signup-errors">
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
-        <form className="signup-form" onSubmit={onSignUp}>
-          <div className="signup-input-container">
-            <label className="signup-label"></label>
-            <input
-              className="signup-input"
-              placeholder="First Name"
-              type="text"
-              name="firstName"
-              onChange={updateFirstName}
-              value={firstName}
-            ></input>
+    <div className="signup-page">
+      <div className="signup-background">
+        <div className="signup-form-container">
+          <div className="logo-container">
+            <h1 className="logo">Instapix</h1>
+            <p>Sign up to see photos and videos from your friends.</p>
+            <button onClick={handleGuestLogin} className="signup-demo-user">
+              <i class="fa-sharp fa-solid fa-user"></i> Log in as a demo user
+            </button>
           </div>
+          <div className="auth-separator">
+            <span>OR</span>
+          </div>
+          <div id="signup-errors">
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <form className="signup-form" onSubmit={onSignUp}>
+            <div className="signup-input-container">
+              <label className="signup-label"></label>
+              <input
+                className="signup-input"
+                placeholder="First Name"
+                type="text"
+                name="firstName"
+                onChange={updateFirstName}
+                value={firstName}
+              ></input>
+            </div>
 
-          <div className="signup-input-container">
-            <label className="signup-label"></label>
-            <input
-              className="signup-input"
-              placeholder="Last Name"
-              type="text"
-              name="lastName"
-              onChange={updateLastName}
-              value={lastName}
-            ></input>
-          </div>
+            <div className="signup-input-container">
+              <label className="signup-label"></label>
+              <input
+                className="signup-input"
+                placeholder="Last Name"
+                type="text"
+                name="lastName"
+                onChange={updateLastName}
+                value={lastName}
+              ></input>
+            </div>
 
-          <div className="signup-input-container">
-            <label className="signup-label"></label>
-            <input
-              className="signup-input"
-              placeholder="Username"
-              type="text"
-              name="username"
-              onChange={updateUsername}
-              value={username}
-            ></input>
+            <div className="signup-input-container">
+              <label className="signup-label"></label>
+              <input
+                className="signup-input"
+                placeholder="Username"
+                type="text"
+                name="username"
+                onChange={updateUsername}
+                value={username}
+              ></input>
+            </div>
+            <div className="signup-input-container">
+              <label className="signup-label"></label>
+              <input
+                className="signup-input"
+                type="text"
+                name="email"
+                onChange={updateEmail}
+                value={email}
+                placeholder="Email"
+              ></input>
+            </div>
+            <div className="signup-input-container">
+              <label className="signup-label"></label>
+              <input
+                className="signup-input"
+                type="password"
+                name="password"
+                onChange={updatePassword}
+                value={password}
+                placeholder="Password"
+              ></input>
+            </div>
+            <button className="signup-button" type="submit">
+              Sign Up
+            </button>
+          </form>
+        </div>
+        <div className="signup-form-container">
+          <div className="login-link-container">
+            <p>
+              Have an account?{" "}
+              <Link className="login-link" to="/login">
+                Log in
+              </Link>
+            </p>
           </div>
-          <div className="signup-input-container">
-            <label className="signup-label"></label>
-            <input
-              className="signup-input"
-              type="text"
-              name="email"
-              onChange={updateEmail}
-              value={email}
-              placeholder="Email"
-            ></input>
-          </div>
-          <div className="signup-input-container">
-            <label className="signup-label"></label>
-            <input
-              className="signup-input"
-              type="password"
-              name="password"
-              onChange={updatePassword}
-              value={password}
-              placeholder="Password"
-            ></input>
-          </div>
-          <button className="signup-button" type="submit">
-            Sign Up
-          </button>
-        </form>
-      </div>
-      <div className="signup-form-container">
-        <div className="login-link-container">
-          <p>
-            Have an account?{" "}
-            <Link className="login-link" to="/login">
-              Log in
-            </Link>
-          </p>
         </div>
       </div>
     </div>
