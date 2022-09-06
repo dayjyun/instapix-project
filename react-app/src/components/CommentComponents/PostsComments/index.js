51834f9b95e26a22d2c0e423fdc161ee4f820569
Jan <janjovellanos@gmail.com>
2022-09-05 18:46:13 -0700

    edit comment modal

--- react-app/src/components/CommentComponents/PostsComments/index.js
+++ react-app/src/components/CommentComponents/PostsComments/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink, useParams } from "react-router-dom";
-// import * as postActions from '../../../store/posts'
+import { NavLink, useHistory, useParams } from "react-router-dom";
+import * as userActions from '../../../store/users';
 import * as commentActions from '../../../store/comments';
 import CreateComment from "../CreateComment";
 import EditCommentModal from "../EditComment";
@@ -14,6 +14,7 @@ const PostsComments = ({ post }) => {
     const comments = useSelector((state) => Object.values(state.comments))
     const [editing, setEditing] = useState(false);
 
+    const history = useHistory();
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -29,6 +30,11 @@ const PostsComments = ({ post }) => {
         return date
     }
 
+    const userProfile = (userId) => {
+        history.push(`/users/${userId}`)
+        history.go(0)
+    }
+
 
 
     return (
@@ -41,8 +47,8 @@ const PostsComments = ({ post }) => {
                             {/* {comment?.user?.profile_image} */}
                             {/* </div> */}
                             <div className="comment-content">
-                                <img className="comment-profile-pic" src={comment?.user?.profile_image} alt='preview'></img>
-                                <div className="comment-username">
+                                <img className="comment-profile-pic" onClick={() => userProfile(comment?.user?.id)} src={comment?.user?.profile_image} alt='preview'></img>
+                                <div className="comment-username" onClick={() => userProfile(comment?.user?.id)}>
                                     {comment?.user?.username}
                                     <div className="comment-date">
                                         {getCreatedDate(comment?.createdAt)}
