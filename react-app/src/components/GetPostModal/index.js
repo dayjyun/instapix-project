7d92ad274f5f0ddda8407046ec097633384bfa28
kevin <kevinbarrios@ymail.com>
2022-09-06 01:56:24 -0500

    fixed num_likes and num_comments spacing

--- react-app/src/components/GetPostModal/index.js
+++ react-app/src/components/GetPostModal/index.js
@@ -9,17 +9,22 @@ function GetPostModal({ post }) {
 
   return (
     <>
-      <button className="explore-post-button-wrap"
+      <button
+        className="explore-post-button-wrap"
         style={{ backgroundColor: "transparent", border: "none" }}
         onClick={() => setShowModal(true)}
       >
         <img className="explore-post-image" src={post?.post_url}></img>
 
         <div className="explore-post-text">
-          <i className="fa-sharp fa-solid fa-heart"></i>
-          <p className="ep-likes">{post?.likes}</p>
-          <i className="fa-sharp fa-solid fa-comment"></i>
-          <p className="ep-comments">{post?.num_comments}</p>
+          <div className="explore-post-text-likes">
+            <i className="fa-sharp fa-solid fa-heart"></i>
+            <p className="ep-likes">{post?.likes}</p>
+          </div>
+          <div className="explore-post-text-comments">
+            <i className="fa-sharp fa-solid fa-comment"></i>
+            <p className="ep-comments">{post?.num_comments}</p>
+          </div>
         </div>
       </button>
       {showModal && (
--- react-app/src/components/PostsComponent/ExplorerPosts/index.js
+++ react-app/src/components/PostsComponent/ExplorerPosts/index.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import { loadAllPosts } from "../../../store/posts";
 import GetPostModal from "../../GetPostModal";
 import "./index.css";
