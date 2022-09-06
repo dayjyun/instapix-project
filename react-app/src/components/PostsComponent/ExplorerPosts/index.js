7646df7ee50528c24ba4c4d194fd9ea3ad352fa1
Kevin Barrios <89671686+dayjyun@users.noreply.github.com>
2022-09-06 03:11:01 -0500

    Revert "Kevin frontend into dev"

--- react-app/src/components/PostsComponent/ExplorerPosts/index.js
+++ react-app/src/components/PostsComponent/ExplorerPosts/index.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { loadAllPosts } from "../../../store/posts";
 import GetPostModal from "../../GetPostModal";
 import "./index.css";
@@ -20,6 +21,12 @@ function ExplorerPosts() {
             <div className="explore-post-card">
               <GetPostModal post={post} />
             </div>
+            {/* <div className="explore-post-text">
+              <i className="fa-sharp fa-solid fa-heart"></i>
+              <p className="ept-likes">{post?.likes}</p>
+              <i className="fa-sharp fa-solid fa-comment"></i>
+              <p className="ept-comments">{post?.num_comments}</p>
+            </div> */}
           </li>
         ))}
       </div>
