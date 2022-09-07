7a866ac1f4c0fef6cfa352b7caaf6e16efb0c4e1
Jan <janjovellanos@gmail.com>
2022-09-06 20:58:22 -0700

    profile posts modal up

--- react-app/src/components/GetPostModal/GetPost.js
+++ react-app/src/components/GetPostModal/GetPost.js
@@ -13,8 +13,6 @@ function GetPost({ post }) {
   const currUser = useSelector((state) => state.session.user);
   const [copyText, setCopyText] = useState("google.com");
 
-  const history = useHistory()
-  const dispatch = useDispatch();
   const inputHandler = (e) => {
     e.preventDefault();
     setCopyText(e.target.value);
