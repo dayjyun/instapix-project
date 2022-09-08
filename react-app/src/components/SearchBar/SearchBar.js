80ccb385b2b49411ceaa4c7ff48a317bab6b620e
kevin <kevinbarrios@ymail.com>
2022-09-08 17:44:50 -0500

    created search bar

---
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,52 @@
+import { useState, ustState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import './SearchBar.css'
+
+function SearchBar() {
+  const users = Object.values((state) => useSelector(state.users));
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState("");
+
+  const userResults = users.filter((user) => {
+    return user?.username?.toLowerCase().includes(search.toLowerCase());
+  });
+
+  const returnResults = userResults.map((user) => {
+    return (
+      <Link
+        to={`/users/${user?.id}`}
+        key={user.id}
+        onClick={() => setSearch("")}
+        className="search-result-link"
+      >
+        <div className="search-result-text">
+          <div>{user?.username}</div>
+        </div>
+      </Link>
+    );
+  });
+
+  return (
+    <div className="search-bar-container">
+      <form>
+        <div className="search-bar">
+          <input
+            className="search-bar-input"
+            type="text"
+            placeholder="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onClick={() => setSearchResults("results-active")}
+            onBlur={() => setSearchResults("")}
+          />
+        </div>
+      </form>
+      <div className={`search-results-box ${searchResults}`}>
+        {returnResults}
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
