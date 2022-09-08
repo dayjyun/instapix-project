2bc3fb45fdb76d50eb63754262b61d5bb9c86234
Sally <sallykim13@gmail.com>
2022-09-08 18:21:23 -0500

    s

--- react-app/src/components/HomePageComponent/index.js
+++ react-app/src/components/HomePageComponent/index.js
@@ -10,6 +10,7 @@ import PostCardModal from "./PostCardModal";
 import * as postActions from "../../store/posts";
 import * as likeActions from "../../store/likes";
 import testingtesting from "./testing";
+import Login from "../LoginComponent";
 
 const uniqueIndex = () => {
     const indexes = [];
@@ -360,83 +361,84 @@ const HomePageComponent = () => {
         );
     } else {
         return (
-            <div className="logged-out-container">
-                <div className="logged-out-content-container">
-                    <img
-                        className="front-page-image"
-                        src="https://instagram-clone-files.s3.us-west-1.amazonaws.com/frontpage.png"
-                        alt="preview"
-                    ></img>
-                    <div className="login-form-container">
-                        <div className="login-form">
-                            <h1>Instapix</h1>
-                            <div className="login-input-container">
-                                <div className="login-box">
-                                    <div className="email">
-                                        <p>Email</p>
-                                    </div>
-                                    <div className="password">
-                                        <input
-                                            type="text"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="login-input-container">
-                                <div className="login-box">
-                                    <div className="email">
-                                        <p>Password</p>
-                                    </div>
-                                    <div className="password">
-                                        <input
-                                            type="password"
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="login-input-container">
-                                <button
-                                    type="submit"
-                                    onSubmit={onSubmit}
-                                    style={style}
-                                    onClick={onLogin}
-                                    className="login-button"
-                                >
-                                    Log In
-                                </button>
-                            </div>
-                            <div className="or-container">
-                                <p className="or1">______________</p>
-                                <p className="or">OR</p>
-                                <p className="or2">______________</p>
-                            </div>
-                            <div className="login-input-container">
-                                <button
-                                    type="submit"
-                                    style={style}
-                                    onClick={handleGuestLogin}
-                                    className="login-button-guest"
-                                >
-                                    Log in as Guest
-                                </button>
-                            </div>
-                        </div>
-                        <div className="sign-up-link">
-                            {/* sign up modal goes here */}
-                            <p>
-                                Don't have an account?
-                                <a href="/sign-up"> Sign up</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Login />
+            // <div className="logged-out-container">
+            //     <div className="logged-out-content-container">
+            //         <img
+            //             className="front-page-image"
+            //             src="https://instagram-clone-files.s3.us-west-1.amazonaws.com/frontpage.png"
+            //             alt="preview"
+            //         ></img>
+            //         <div className="login-form-container">
+            //             <div className="login-form">
+            //                 <h1>Instapix</h1>
+            //                 <div className="login-input-container">
+            //                     <div className="login-box">
+            //                         <div className="email">
+            //                             <p>Email</p>
+            //                         </div>
+            //                         <div className="password">
+            //                             <input
+            //                                 type="text"
+            //                                 value={email}
+            //                                 onChange={(e) => setEmail(e.target.value)}
+            //                                 required
+            //                             />
+            //                         </div>
+            //                     </div>
+            //                 </div>
+            //                 <div className="login-input-container">
+            //                     <div className="login-box">
+            //                         <div className="email">
+            //                             <p>Password</p>
+            //                         </div>
+            //                         <div className="password">
+            //                             <input
+            //                                 type="password"
+            //                                 value={password}
+            //                                 onChange={(e) => setPassword(e.target.value)}
+            //                                 required
+            //                             />
+            //                         </div>
+            //                     </div>
+            //                 </div>
+            //                 <div className="login-input-container">
+            //                     <button
+            //                         type="submit"
+            //                         onSubmit={onSubmit}
+            //                         style={style}
+            //                         onClick={onLogin}
+            //                         className="login-button"
+            //                     >
+            //                         Log In
+            //                     </button>
+            //                 </div>
+            //                 <div className="or-container">
+            //                     <p className="or1">______________</p>
+            //                     <p className="or">OR</p>
+            //                     <p className="or2">______________</p>
+            //                 </div>
+            //                 <div className="login-input-container">
+            //                     <button
+            //                         type="submit"
+            //                         style={style}
+            //                         onClick={handleGuestLogin}
+            //                         className="login-button-guest"
+            //                     >
+            //                         Log in as Guest
+            //                     </button>
+            //                 </div>
+            //             </div>
+            //             <div className="sign-up-link">
+            //                 {/* sign up modal goes here */}
+            //                 <p>
+            //                     Don't have an account?
+            //                     <a href="/sign-up"> Sign up</a>
+            //                 </p>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>
         );
     }
 };
