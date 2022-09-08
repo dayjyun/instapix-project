523fe8fa9cc44487bd2efca86c371600d2f106a7
Sally <sallykim13@gmail.com>
2022-09-07 21:12:53 -0500

    d

--- react-app/src/components/UserComponent/index.js
+++ react-app/src/components/UserComponent/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import FollowModal from '../FollowModal/FollowModal';
@@ -21,6 +21,16 @@ function User() {
   const posts = useSelector(state => Object.values(state.posts))
   const usersPosts = posts.filter(post => post.user_id === user.id)
 
+  const [onMyPage, setOnMyPage] = useState('');
+  const [alreadyFollowing, setAlreadyFollowing] = useState('')
+
+  useEffect(() => {
+    if (user?.id === loggedUser?.id) {
+      setOnMyPage(true)
+    }
+  }, [user, loggedUser])
+
+
   useEffect(() => {
     dispatch(getOneUser(parseInt(userId)))
   }, [dispatch, userId])
@@ -40,6 +50,11 @@ function User() {
     dispatch(loadAllPosts())
   }, [dispatch])
 
+
+  const isFollowing = () => {
+    setAlreadyFollowing(true)
+  }
+
   return (
     <>
       {user && (
@@ -50,12 +65,21 @@ function User() {
             </div>
 
             <div className='user-info-info'>
-              <h2 className='h2-style-username'>{user?.username}</h2>
+              <div className='header-line-styling'>
+                <h2 className='h2-style-username'>{user?.username}</h2>
+
+                {!onMyPage && (
+                  <div className='follow-btn'>
+                    <button className='follow-btn-styling'>Follow</button></div>
+                )}
+
+              </div>
 
               <div className='user-stat-box'>
                 <div className='post-count'><p><span className='bold'>{user?.num_posts}</span> posts</p></div>
 
                 {follows?.followers && (
+
                   <div className='post-count pointer'>
                     <FollowerModal user={user} />
                   </div>
