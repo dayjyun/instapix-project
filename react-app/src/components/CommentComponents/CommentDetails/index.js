e0204296dbbbf48e92918b04b1251227f5087a01
kevin <kevinbarrios@ymail.com>
2022-09-08 23:15:31 -0500

    changes

--- react-app/src/components/CommentComponents/CommentDetails/index.js
+++ react-app/src/components/CommentComponents/CommentDetails/index.js
@@ -1,64 +1,63 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams, useHistory } from 'react-router-dom';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useHistory } from "react-router-dom";
 
-import * as commentActions from '../../../store/comments';
-import './CommentDetails.css'
+import * as commentActions from "../../../store/comments";
+import "./CommentDetails.css";
 
 const CommentDetails = () => {
-    const { commentId } = useParams();
-    const user = useSelector(state => (state.session.user));
-    const comment = useSelector(state => (state.comments[commentId]));
+  const { commentId } = useParams();
+  const user = useSelector((state) => state.session.user);
+  const comment = useSelector((state) => state.comments[commentId]);
 
-    const history = useHistory();
-    const dispatch = useDispatch();
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(commentActions.getComment(commentId))
-    }, [dispatch, commentId])
-
-
-    const handleDeleteBtn = (commentId) => {
-        dispatch(commentActions.removeComment(commentId));
-        history.push(`/comments/${commentId}`);
+  useEffect(() => {
+    if (user) {
+      dispatch(commentActions.getComment(commentId));
     }
+  }, [dispatch, commentId, user]);
 
-    let commentEditBtns;
+  const handleDeleteBtn = (commentId) => {
+    dispatch(commentActions.removeComment(commentId));
+    history.push(`/comments/${commentId}`);
+  };
 
-    if (comment?.user_id === user?.id) {
-        commentEditBtns = (
-            <>
-                <button onClick={() => history.push(`/comments/${commentId}/edit`)}>Edit</button>
-                <button onClick={() => handleDeleteBtn(commentId)}>Delete</button>
-            </>
-        );
-    }
+  let commentEditBtns;
 
-    if (!comment) {
-        return (
-            <h1>Comment NOT Found</h1>
-        )
-    }
-
-    return (
-        <>
-            <div>
-                <div>
-                    <div>
-                        <h1>Details for Comment {commentId}</h1>
-                            <div>
-                                <h4>USERID: {comment?.user_id}</h4>
-                                <h4>POSTID: {comment?.post_id}</h4>
-                                <h4>BODY: {comment?.body}</h4>
-                            </div>
-                        <div className='edit-btns'>
-                            {commentEditBtns}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
+  if (comment?.user_id === user?.id) {
+    commentEditBtns = (
+      <>
+        <button onClick={() => history.push(`/comments/${commentId}/edit`)}>
+          Edit
+        </button>
+        <button onClick={() => handleDeleteBtn(commentId)}>Delete</button>
+      </>
     );
+  }
+
+  if (!comment) {
+    return <h1>Comment NOT Found</h1>;
+  }
+
+  return (
+    <>
+      <div>
+        <div>
+          <div>
+            <h1>Details for Comment {commentId}</h1>
+            <div>
+              <h4>USERID: {comment?.user_id}</h4>
+              <h4>POSTID: {comment?.post_id}</h4>
+              <h4>BODY: {comment?.body}</h4>
+            </div>
+            <div className="edit-btns">{commentEditBtns}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CommentDetails;
--- react-app/src/components/CommentComponents/PostsComments/index.js
+++ react-app/src/components/CommentComponents/PostsComments/index.js
@@ -1,117 +1,131 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import * as likeActions from '../../../store/likes';
-import * as commentActions from '../../../store/comments';
+import * as likeActions from "../../../store/likes";
+import * as commentActions from "../../../store/comments";
 import CreateComment from "../CreateComment";
 import EditCommentModal from "../EditComment";
-import './PostComments.css'
+import "./PostComments.css";
 import LikesModal from "../../LikesModal";
 
 export const getCreatedDate = (datestr) => {
-    const fullDate = new Date(datestr).toDateString()
-    let date = fullDate.slice(4)
-    if (date[4] === '0') {
-        date = date.slice(0, 4) + date.slice(5);
-    };
-    return date
-}
+  const fullDate = new Date(datestr).toDateString();
+  let date = fullDate.slice(4);
+  if (date[4] === "0") {
+    date = date.slice(0, 4) + date.slice(5);
+  }
+  return date;
+};
 
 const PostsComments = ({ post, setCurrPost }) => {
-    const user = useSelector(state => state.session.user)
-    const comments = useSelector((state) => Object.values(state.comments));
-    const likes = useSelector(state => Object.values(state.likes))
-    const likesUserIds = post?.real_likes?.map(like => like?.user_id);
-    const [liked, setLiked] = useState(false);
-    const inputEl = useRef(null);
+  const user = useSelector((state) => state.session.user);
+  const comments = useSelector((state) => Object.values(state.comments));
+  const likes = useSelector((state) => Object.values(state.likes));
+  const likesUserIds = post?.real_likes?.map((like) => like?.user_id);
+  const [liked, setLiked] = useState(false);
+  const inputEl = useRef(null);
 
-    const history = useHistory();
-    const dispatch = useDispatch();
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        currUserLiked()
-        dispatch(commentActions.loadPostComments(post?.id))
-        dispatch(likeActions.fetchLike(post?.id))
-    }, [dispatch, post])
-
-    const userProfile = (userId) => {
-        history.push(`/users/${userId}`)
-        history.go(0)
+  useEffect(() => {
+    if (user) {
+      currUserLiked();
+      dispatch(commentActions.loadPostComments(post.id));
+      dispatch(likeActions.fetchLike(post.id));
     }
+  }, [dispatch, post]);
 
-    const currUserLiked = () => {
-        setLiked(likesUserIds?.includes(user?.id))
+  const userProfile = (userId) => {
+    history.push(`/users/${userId}`);
+    history.go(0);
+  };
+
+  const currUserLiked = () => {
+    setLiked(likesUserIds?.includes(user?.id));
+  };
+
+  const likePost = async () => {
+    if (likesUserIds?.includes(user?.id)) {
+      await dispatch(likeActions.unlike(post?.id));
+    } else {
+      await dispatch(likeActions.like(post?.id));
     }
+  };
 
-    const likePost = async () => {
-        if (likesUserIds?.includes(user?.id)) {
-            await dispatch(likeActions.unlike(post?.id))
+  let postLiked = (
+    <i className="fa-regular fa-solid fa-heart heart-likes-solid"></i>
+  );
+  let postNotLiked = <i className="fa-regular fa-heart heart-likes-hollow"></i>;
 
-        } else {
-            await dispatch(likeActions.like(post?.id))
-        }
-    };
+  comments?.sort((a, b) => {
+    return b.id - a.id;
+  });
 
-    let postLiked = (<i className="fa-regular fa-solid fa-heart heart-likes-solid"></i>)
-    let postNotLiked = (<i className="fa-regular fa-heart heart-likes-hollow"></i>)
-
-    comments?.sort((a, b) => {
-        return b.id - a.id;
-    })
-
-    return (
-        <>
-            <div className="post-comments-container">
-                <ul className="comment-card-list">
-                    {comments?.map((comment) => (
-                        <li className='comment-card-container' key={comment?.id}>
-                            {/* <div className="comment-profile-pic"> */}
-                            {/* {comment?.user?.profile_image} */}
-                            {/* </div> */}
-                            <div className="comment-content">
-                                <img className="comment-profile-pic" onClick={() => userProfile(comment?.user?.id)} src={comment?.user?.profile_image} alt='preview'></img>
-                                <div className="comment-username" onClick={() => userProfile(comment?.user?.id)}>
-                                    {comment?.user?.username}
-                                    <div className="comment-date">
-                                        {getCreatedDate(comment?.createdAt)}
-                                    </div>
-                                </div>
-                                <div className="comment-body">
-                                    {comment?.body}
-                                    {comment?.user_id === user?.id &&
-                                        <div className="edit-comment-container">
-                                            <EditCommentModal comment={comment} />
-                                        </div>
-                                    }
-                                </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-                <div>
-                    <div className="likes-comment-container">
-                        <div className="heart-comment-bubble">
-                            <div onClick={async () => await likePost()
-                                .then(async () => setLiked(!liked))
-                                .then(async () => await setCurrPost(post))}>
-                                {liked ? postLiked : postNotLiked}
-                            </div>
-                            <div
-                                onClick={() => inputEl.current.focus()}>
-                                <i className="fa-regular fa-comment comment-bubble"></i>
-                            </div>
-                        </div>
-                        <div className="post-likes">
-                            {/* {likes?.length} likes */}
-                            <LikesModal likes={likes}/>
-                            </div>
-                        <div className="post-date">{getCreatedDate(post?.created_at)}</div>
-                    </div>
-                    <CreateComment inputEl={inputEl} post={post} />
+  return (
+    <>
+      <div className="post-comments-container">
+        <ul className="comment-card-list">
+          {comments?.map((comment) => (
+            <li className="comment-card-container" key={comment?.id}>
+              {/* <div className="comment-profile-pic"> */}
+              {/* {comment?.user?.profile_image} */}
+              {/* </div> */}
+              <div className="comment-content">
+                <img
+                  className="comment-profile-pic"
+                  onClick={() => userProfile(comment?.user?.id)}
+                  src={comment?.user?.profile_image}
+                  alt="preview"
+                ></img>
+                <div
+                  className="comment-username"
+                  onClick={() => userProfile(comment?.user?.id)}
+                >
+                  {comment?.user?.username}
+                  <div className="comment-date">
+                    {getCreatedDate(comment?.createdAt)}
+                  </div>
                 </div>
+                <div className="comment-body">
+                  {comment?.body}
+                  {comment?.user_id === user?.id && (
+                    <div className="edit-comment-container">
+                      <EditCommentModal comment={comment} />
+                    </div>
+                  )}
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <div className="likes-comment-container">
+            <div className="heart-comment-bubble">
+              <div
+                onClick={async () =>
+                  await likePost()
+                    .then(async () => setLiked(!liked))
+                    .then(async () => await setCurrPost(post))
+                }
+              >
+                {liked ? postLiked : postNotLiked}
+              </div>
+              <div onClick={() => inputEl.current.focus()}>
+                <i className="fa-regular fa-comment comment-bubble"></i>
+              </div>
             </div>
-        </>
-    )
-}
+            <div className="post-likes">
+              {/* {likes?.length} likes */}
+              <LikesModal likes={likes} />
+            </div>
+            <div className="post-date">{getCreatedDate(post?.created_at)}</div>
+          </div>
+          <CreateComment inputEl={inputEl} post={post} />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default PostsComments
+export default PostsComments;
