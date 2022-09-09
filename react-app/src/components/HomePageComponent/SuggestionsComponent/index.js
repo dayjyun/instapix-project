912112cd49c92eab71098cf3b7754c35fd90f75e
Huydu Tran <duutran87@icloud.com>
2022-09-08 23:00:11 -0700

    seperated all components

--- react-app/src/components/HomePageComponent/SuggestionsComponent/index.js
+++ react-app/src/components/HomePageComponent/SuggestionsComponent/index.js
@@ -4,27 +4,28 @@ import { getFollowingBackendHome } from "../../../store/follow"
 import { getAllUsers } from "../../../store/users"
 import { useHistory } from "react-router-dom"
 
-const SuggestionsComponent = () => {
+const SuggestionsComponent = ({ i2 }) => {
     const dispatch = useDispatch()
     const history = useHistory()
-    let following = useSelector(state => state.follow)
+    // let following = useSelector(state => state.follow)
     const sessionUser = useSelector(state => state.session.user)
     const allUsers = Object.values(useSelector(state => state.users))
     const [nonFollowers, setNonFollowers] = useState([])
+
     // following && console.log(Object.values(following))
     // let following2 = following?.follows
     // following && console.log(following)
-    following && (following = Object.values(following)?.map(following => following?.follower_info?.id))
-    let usersNotFollowing;
-    console.log("NON FOLLOWERS", nonFollowers)
+    // following && (following = Object.values(following)?.map(following => following?.follower_info?.id))
+    // let usersNotFollowing;
+    // console.log("NON FOLLOWERS", nonFollowers)
 
-    useEffect(() => {
-        if (allUsers && following && nonFollowers.length === 0) {
-            // console.log("users", allUsers, "FOLLOWING", following)
-            usersNotFollowing = allUsers.filter(user => !following?.includes(user.id))
-            setNonFollowers(usersNotFollowing)
-        }
-    }, [allUsers, following])
+    // useEffect(() => {
+    //     if (allUsers && following && nonFollowers.length === 0) {
+    //         // console.log("users", allUsers, "FOLLOWING", following)
+    //         usersNotFollowing = allUsers.filter(user => !following?.includes(user.id))
+    //         setNonFollowers(usersNotFollowing)
+    //     }
+    // }, [allUsers, following])
 
     useEffect(() => {
         dispatch(getAllUsers())
@@ -51,21 +52,21 @@ const SuggestionsComponent = () => {
         }
     }
 
-    const ProfileImageTagSmall = (user) => {
-        if (user?.profile_image) {
+    const ProfileImageTagSmall = (i) => {
+        if (allUsers[i]?.profile_image) {
             return (
                 <div className="profile-button-large" onClick={e => {
                     e.preventDefault()
-                    history.push(`/users/${user?.id}`)
+                    history.push(`/users/${allUsers[i]?.id}`)
                 }}>
-                    <img style={{ width: '2.5em', height: '2.5em', marginLeft: '-.2em' }} className='profile-img-circle-container' src={user?.profile_image} alt='preview'></img>
+                    <img style={{ width: '2.5em', height: '2.5em', marginLeft: '-.2em' }} className='profile-img-circle-container' src={allUsers[i]?.profile_image} alt='preview'></img>
                 </div>
             )
         } else {
             return (
                 <div style={{ marginTop: '-.1em' }} onClick={e => {
                     e.preventDefault()
-                    history.push(`/users/${user?.id}`)
+                    history.push(`/users/${allUsers[i]?.id}`)
                 }} className='fa-regular fa-user-circle fa-xl'></div>
             )
         }
@@ -77,22 +78,20 @@ const SuggestionsComponent = () => {
                 {ProfileImageTagLarge()}
                 <div className="suggestions-username-name">
                     <a className="suggestions-username" href={`/users/${sessionUser?.id}`}>{sessionUser?.username}</a>
-                    <span>{sessionUser.first_name}</span>
+                    <span>{sessionUser?.first_name}</span>
                 </div>
             </div>
             <p className="suggestions-for-u">Suggestions For You</p>
             <div className="suggestions-users-containers">
-                {nonFollowers.length > 0 && nonFollowers.map((user, i) => {
-
+                {i2?.map((i, index) => {
                     return (
-                        <div key={i} className="suggestions-user-card">
-                            {ProfileImageTagSmall(user)}
+                        <div key={index} className="suggestions-user-card">
+                            {ProfileImageTagSmall(i)}
                             <div className="suggestions-username-name">
-                                <a className="suggestions-username" href={`/users/${user?.id}`}>{user?.username}</a>
+                                <a className="suggestions-username" href={`/users/${allUsers[i]?.id}`}>{allUsers[i]?.username}</a>
                                 <span style={{ fontSize: '14px' }}>Popular</span>
                             </div>
                             <div className="user-card-follow-btn">
-                                <span>Follow</span>
                             </div>
                         </div>
                     )
--- react-app/src/components/HomePageComponent/index.js
+++ react-app/src/components/HomePageComponent/index.js
@@ -1,21 +1,13 @@
-import { useState, useEffect } from "react"
-import { useHistory } from "react-router-dom"
+import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import * as userActions from '../../store/users'
 import * as followingActions from '../../store/follow'
 import './HomePageComponent.css'
-// import PostCardButtons from "./PostCardModal/PostCardButtons"
-import PostCardModal from "./PostCardModal"
-// import * as postActions from '../../store/posts'
 import { getFollowingPosts } from "../../store/posts"
-import LikeComponent from "./LikeComponent"
-import TotalLikesComponent from "./LikeComponent/TotalLikesComponent"
-import { getCreatedDate } from "../CommentComponents/PostsComments"
-import { FeedPostModalCommentBtn, FeedPostModalViewStr } from "./FeedPostModal"
 import Login from "../LoginComponent"
 import SuggestionsComponent from "./SuggestionsComponent"
-
-// import LikeCommentComponent from "../LikeCommentComponent"
+import TrendingUsers from "./TrendingUsersComponent"
+import FeedPostsComponent from "./FeedPostsSection"
 
 const uniqueIndex = () => {
   const indexes = []
@@ -29,21 +21,13 @@ const uniqueIndex = () => {
 }
 let i;
 let i2;
-//
-// const [currPostState, setCurrPostState] = useState(posts);
 
 const HomePageComponent = () => {
   const dispatch = useDispatch()
-  const history = useHistory()
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [style, setStyle] = useState({})
-  const [nonFollowers, setNonFollowers] = useState([])
 
   const sessionUser = useSelector(state => state.session.user)
   const allUsers = Object.values(useSelector(state => state.users))
-  const posts = Object.values(useSelector(state => state.posts))
+
   // let following = useSelector(state => state.follow)
   // following && console.log(Object.values(following))
   // let following2 = following?.follows
@@ -65,12 +49,6 @@ const HomePageComponent = () => {
   }, [])
 
   useEffect(() => {
-    if (email && password) {
-      setStyle({ backgroundColor: 'rgb(42, 126, 187' })
-    }
-  }, [email, password])
-
-  useEffect(() => {
     if (sessionUser) {
       dispatch(userActions.getAllUsers())
       dispatch(getFollowingPosts())
@@ -79,171 +57,15 @@ const HomePageComponent = () => {
     }
   }, [dispatch, sessionUser])
 
-  const reset = () => {
-    setEmail("")
-    setPassword('')
-    setStyle({})
-  }
-
-
-  // const ProfileImageTagLarge = () => {
-  //   if (sessionUser?.profile_image) {
-  //     return (
-  //       <div className="profile-button-large" onClick={e => {
-  //         e.preventDefault()
-  //         history.push(`/users/${sessionUser?.id}`)
-  //       }}>
-  //         <img style={{ width: '4em', height: '4em', marginLeft: '-.2em' }} className='profile-img-circle-container' src={sessionUser?.profile_image} alt='preview'></img>
-  //       </div>
-  //     )
-  //   } else {
-  //     return (
-  //       <div style={{ marginTop: '-.1em' }} onClick={e => {
-  //         e.preventDefault()
-  //         history.push(`/users/${sessionUser?.id}`)
-  //       }} className='fa-regular fa-user-circle fa-xl'></div>
-  //     )
-  //   }
-  // }
-
-  // const ProfileImageTagSmall = (user) => {
-  //   if (user?.profile_image) {
-  //     return (
-  //       <div className="profile-button-large" onClick={e => {
-  //         e.preventDefault()
-  //         history.push(`/users/${user?.id}`)
-  //       }}>
-  //         <img style={{ width: '2.5em', height: '2.5em', marginLeft: '-.2em' }} className='profile-img-circle-container' src={user?.profile_image} alt='preview'></img>
-  //       </div>
-  //     )
-  //   } else {
-  //     return (
-  //       <div style={{ marginTop: '-.1em' }} onClick={e => {
-  //         e.preventDefault()
-  //         history.push(`/users/${user?.id}`)
-  //       }} className='fa-regular fa-user-circle fa-xl'></div>
-  //     )
-  //   }
-  // }
-
-  const ProfileImageTagSmallCard = (post) => {
-    if (post?.User?.profile_image) {
-      return (
-        <div className="profile-button-large-2" onClick={e => {
-          e.preventDefault()
-          history.push(`/users/${post?.User?.id}`)
-        }}>
-          <img style={{ width: '2.5em', height: '2.5em', marginLeft: '-.2em' }} className='profile-img-circle-container' src={post?.User?.profile_image} alt='preview'></img>
-        </div>
-      )
-    } else {
-      return (
-        <div style={{ marginTop: '-.1em' }} onClick={e => {
-          e.preventDefault()
-          history.push(`/users/${post?.User?.id}`)
-        }} className='fa-regular fa-user-circle fa-xl'></div>
-      )
-    }
-  }
-
   if (sessionUser) {
     return (
       <div className='home-page-container'>
         <div className="home-content-container">
           <div className="users-container">
-            <div className="users-section">
-              <div className="trending">
-                <p>Trending 🔥🔥🔥</p>
-              </div>
-              <div className="user-pics-container">
-                {i?.map(i => (
-                  <div key={i} className="user-pics">
-                    <button onClick={
-                      e => {
-                        e.preventDefault()
-                        history.push(`/users/${allUsers[i]?.id}`)
-                      }
-                    }>
-                      <img className="users-img-circle-container" src={allUsers[i]?.profile_image}>
-                      </img>
-                    </button>
-                  </div>
-                ))}
-              </div>
-              <div className="user-pics-container">
-                {i?.map(i => (
-                  <div key={i} className="username">
-                    <a href={`/users/${allUsers[i]?.id}`}>{allUsers[i]?.username}</a>
-                  </div>
-                ))}
-              </div>
-            </div>
-            <div className="feed-section">
-              {posts && (Object.values(posts)?.map(post => {
-                // const randomPost = posts[Math.floor(Math.random() * posts?.length)]
-                return (
-                  <div key={post?.id} className="feed-post-container">
-                    <div className="feed-username-container">
-                      {ProfileImageTagSmallCard(post)}
-                      <a href={`/users/${post?.User?.id}`}>{post?.User?.username}</a>
-                      <div>
-                        <PostCardModal
-                          post={post} randomPost={post} />
-                      </div>
-                    </div>
-                    <div className="feed-post-image">
-                      <img className="feed-image" src={post?.post_url} alt="Post has no image"></img>
-                    </div>
-                    <div className="feed-like-container">
-                      <LikeComponent post={post} /> <FeedPostModalCommentBtn post={post} />
-                    </div>
-                    <TotalLikesComponent post={post} />
-                    <span className='feed-caption'>{post?.User?.username}
-                      <span style={{ fontWeight: '400' }}> {post?.caption}</span>
-                    </span>
-                    <div className="load-comments-button"><FeedPostModalViewStr post={post} /></div>
-                    <div className="feed-post-date">{getCreatedDate(post?.created_at)}</div>
-                  </div>
-                )
-              }))}
-              <h1>add as much as needed</h1>
-              <h1>add as much as needed</h1>
-              <h1>add as much as needed</h1>
-              <h1>add as much as needed</h1>
-              <h1>add as much as needed</h1>
-            </div>
+            <TrendingUsers i={i} />
+            <FeedPostsComponent />
           </div>
-          <SuggestionsComponent />
-          {/* <div className="suggestions-container">
-            <div className="suggestions-username-container">
-              {ProfileImageTagLarge()}
-              <div className="suggestions-username-name">
-                <a className="suggestions-username" href={`/users/${sessionUser?.id}`}>{sessionUser?.username}</a>
-                <span>{sessionUser.first_name}</span>
-              </div>
-            </div>
-            <p className="suggestions-for-u">Suggestions For You</p>
-            <div className="suggestions-users-containers">
-              {nonFollowers.length > 0 && nonFollowers.map(user => {
-                // let followText = 'Follow'
-                return (
-                  <div key={i} className="suggestions-user-card">
-                    {ProfileImageTagSmall(user)}
-                    <div className="suggestions-username-name">
-                      <a className="suggestions-username" href={`/users/${user?.id}`}>{user?.username}</a>
-                      <span style={{ fontSize: '14px' }}>Popular</span>
-                    </div>
-                    <div className="user-card-follow-btn">
-                      <span>Follow</span>
-                    </div>
-                  </div>
-                )
-              })}
-              <div style={{ marginLeft: '.8em' }}>
-                <p className="copyright">© 2022 INSTAPIX FROM FELIPE SALLY JAN KEVIN HUYDU</p>
-              </div>
-            </div>
-          </div> */}
+          <SuggestionsComponent i2={i2} />
         </div >
       </div >
     )
