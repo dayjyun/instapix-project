963ce2c5c6a0cf2c63e9c2f789460a5e78dcdeaa
Huydu Tran <duutran87@icloud.com>
2022-09-08 23:29:40 -0700

    testing

---
+++ react-app/src/components/HomePageComponent/TrendingUsersComponent/index.js
@@ -0,0 +1,40 @@
+import { useSelector } from "react-redux"
+import { useHistory } from "react-router-dom"
+
+const TrendingUsers = ({ i }) => {
+    const history = useHistory()
+
+    const allUsers = Object.values(useSelector(state => state.users))
+
+    return (
+        <div className="users-section">
+            <div className="trending">
+                <p>Trending 🔥🔥🔥</p>
+            </div>
+            <div className="user-pics-container">
+                {i?.map(i => (
+                    <div key={i} className="user-pics">
+                        <button onClick={
+                            e => {
+                                e.preventDefault()
+                                history.push(`/users/${allUsers[i]?.id}`)
+                            }
+                        }>
+                            <img className="users-img-circle-container" src={allUsers[i]?.profile_image}>
+                            </img>
+                        </button>
+                    </div>
+                ))}
+            </div>
+            <div className="user-pics-container">
+                {i?.map(i => (
+                    <div key={i} className="username">
+                        <a href={`/users/${allUsers[i]?.id}`}>{allUsers[i]?.username}</a>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default TrendingUsers
