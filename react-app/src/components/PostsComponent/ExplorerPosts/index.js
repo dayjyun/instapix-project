95f418f83a3367d03ea0679b34f37c8fa44d2fc3
kevin <kevinbarrios@ymail.com>
2022-09-10 04:25:33 -0500

    final updates for the night

--- react-app/src/components/PostsComponent/ExplorerPosts/index.js
+++ react-app/src/components/PostsComponent/ExplorerPosts/index.js
@@ -14,7 +14,6 @@ function ExplorerPosts() {
     dispatch(loadAllPosts());
   }, [dispatch, likes, comments]);
 
-
   return (
     <div className="explore-page">
       <div className="explore-post">
