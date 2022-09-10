2b64470356be41696b11075a8fe3c0047acc2a59
kevin <kevinbarrios@ymail.com>
2022-09-10 14:39:39 -0500

    clean up code

--- react-app/src/components/SearchBar/SearchBar.js
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import { allUsers } from "../../store/allUsers";
