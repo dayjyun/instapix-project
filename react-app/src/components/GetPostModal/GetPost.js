2a649af04cc1f07630ee3aed8662adc53897e4d5
Huydu Tran <duutran87@icloud.com>
2022-09-10 14:21:33 -0700

    fixed css

--- react-app/src/components/GetPostModal/GetPost.js
+++ react-app/src/components/GetPostModal/GetPost.js
@@ -32,11 +32,11 @@ function GetPost({ post, user }) {
             }}>
             <img
               className="comment-profile-pic"
-              src={user?.profile_image}
+              src={post?.User?.profile_image}
               alt="preview"
             ></img>
             <div className="post-username-text">
-              {user?.username}
+              {post?.User?.username}
             </div>
           </div>
           {editPostBtn}
@@ -48,8 +48,9 @@ function GetPost({ post, user }) {
               history.push(`/users/${post?.user_id}`)
             }}
             className="comment-profile-pic"
-            src={user?.profile_image}
+            src={post?.User?.profile_image}
             alt="preview"
+            style={{ marginTop: '.5em' }}
           ></img>
           <div
             className="post-username-text"
@@ -57,7 +58,7 @@ function GetPost({ post, user }) {
               e.preventDefault()
               history.push(`/users/${post?.user_id}`)
             }}>
-            {user?.username}
+            {post?.User?.username}
             <div className="posted-date">{getCreatedDate(post?.created_at)}</div>
           </div>
           <div className="caption-container">{post?.caption}</div>
