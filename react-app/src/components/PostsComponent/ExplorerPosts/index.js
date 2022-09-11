464a5187d3acee3a181ba2374f14a339d7a6f075
Huydu Tran <duutran87@icloud.com>
2022-09-10 17:01:03 -0700

    a

--- react-app/src/components/PostsComponent/ExplorerPosts/index.js
+++ react-app/src/components/PostsComponent/ExplorerPosts/index.js
@@ -4,39 +4,23 @@ import { loadAllPosts } from "../../../store/posts";
 import GetPostModal from "../../GetPostModal";
 import "./index.css";
 
-const uniqueIndex = () => {
-  const indexes = [];
-  while (indexes.length !== 25) {
-    const randomIndex = Math.floor(Math.random() * 30);
-    if (!indexes.includes(randomIndex)) {
-      indexes.push(randomIndex);
-    }
-  }
-  return indexes;
-};
-
-let i;
-
 function ExplorerPosts() {
   const dispatch = useDispatch();
   const posts = Object.values(useSelector((state) => state.posts));
-  const likes = useSelector((state) => state.likes);
-  const comments = useSelector((state) => state.comments);
+  const likes = useSelector(state => state.likes)
+  const comments = useSelector(state => state.comments)
 
   useEffect(() => {
     dispatch(loadAllPosts());
   }, [dispatch, likes, comments]);
-  useEffect(() => {
-    i = uniqueIndex();
-  }, []);
 
   return (
     <div className="explore-page">
       <div className="explore-post">
-        {i?.map((i, index) => (
-          <li key={index} className="explore-post-wrap">
+        {posts?.map((post) => (
+          <li key={post?.id} className="explore-post-wrap">
             <div className="explore-post-card">
-              <GetPostModal post={posts[i]} user={posts[i]?.User} />
+              <GetPostModal post={post} user={post?.User} />
             </div>
           </li>
         ))}
