f6d3e54515806dd62b6074c4efd204418a2723d7
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-11-26 13:40:49 -0800

    removed hash router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch, HashRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import NavBar from "./components/Navbar/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
